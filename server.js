502224af2d2078ee5349f8f13ea76ef53293c49a
Allie <allie@codefellows.com>
2018-05-21 11:41:40 -0700

    updating to remove CLIENT_URL

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const pg = require('pg');
 
 const app = express();
 const PORT = process.env.PORT;
-const CLIENT_URL = process.env.CLIENT_URL;
 
 const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
@@ -22,11 +21,11 @@ app.get('/tasks', (req, res) => {
     .catch(console.error);
 });
 
-app.get('*', (req, res) => res.redirect(CLIENT_URL));
+app.get('*', (req, res) => res.status(403).send('This route does not exist'));
+
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
 // PORT=3000
-// CLIENT_URL=http://localhost:8080
 
 // Mac:
 // DATABASE_URL=postgres://localhost:5432/task_app
