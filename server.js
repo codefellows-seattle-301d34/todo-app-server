0fa3f7e9af082fabe46a99a95372308741b49c64
Allie <allie@codefellows.com>
2018-05-23 12:56:13 -0700

    adding day 13 demo

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const pg = require('pg');
 
 const app = express();
 const PORT = process.env.PORT;
+const CLIENT_URL = process.env.CLIENT_URL;
+const TOKEN = process.env.TOKEN;
 
 const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
@@ -21,14 +23,25 @@ app.get('/tasks', (req, res) => {
     .catch(console.error);
 });
 
-app.get('*', (req, res) => res.status(403).send('This route does not exist'));
+app.get('/admin', (req, res) => res.send(TOKEN === parseInt(req.query.token)))
+// app.get('/admin', (req, res) => console.log(req.query.token))
+
+app.get('*', (req, res) => res.redirect(CLIENT_URL));
+
+app.post('/tasks/add', express.urlencoded(), (req, res) => {
+  let {title, description, category, contact, status} = req.body;
+
+  client.query(`
+      INSERT INTO tasks(title, description, category, contact, status)
+      VALUES ($1, $2, $3, $4, $5)`,
+    [title, description, category, contact, status]
+  )
+    .then(results => res.sendStatus(201))
+    .catch(console.error);
+});
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
 // PORT=3000
-
-// Mac:
+// CLIENT_URL=http://localhost:8080
 // DATABASE_URL=postgres://localhost:5432/task_app
-
-// Windows:
-// DATABASE_URL=postgres://USER:PASSWORD@localhost:5432/task_app
